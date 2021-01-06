763b6ff007ff9abb790dad47fdee16358f07adbd
kimnigang <zeus2424797@gmail.com>
2021-01-06 15:40:46 +0900

    login page

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,8 +1,12 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_DB_URL,
-  databaseURL: process.env.FIREBASE_PROJECT_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_DB_URL,
+  databaseURL: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp;
